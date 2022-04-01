italia/publiccode-parser-go

commit f2ba6058ac098230a3192209e1047de0dfdbaee9

chore: add go-bindata to file-freshness' suggested command

.github/workflows/file-freshness.js
@@ -33,7 +33,8 @@ await $`curl -sL ${AMMINISTRAZIONI_TXT_URL} |
   tee /tmp/amministrazioni.txt |
   tail -n +2 |
   cut -f1 |
-  LC_COLLATE=C sort > /tmp/ipa_codes.txt.new`;
+  LC_COLLATE=C sort > /tmp/ipa_codes.txt.new &&
+  go-bindata -pkg assets -o assets/assets.go data/...`;
 
 const latestHash = await md5("file:///tmp/ipa_codes.txt.new");
 

commit 850e2a0554ff2d5a390052772c5fb78b0a3f325a

fix: remove go-bindata from the file-freshness command
Fix bug introduced by f2ba6058a: go-bindata is supposed to be
only suggested in the issue message and not in the actual command.

.github/workflows/file-freshness.js
@@ -33,8 +33,8 @@ await $`curl -sL ${AMMINISTRAZIONI_TXT_URL} |
   tee /tmp/amministrazioni.txt |
   tail -n +2 |
   cut -f1 |
-  LC_COLLATE=C sort > /tmp/ipa_codes.txt.new &&
-  go-bindata -pkg assets -o assets/assets.go data/...`;
+  LC_COLLATE=C sort > /tmp/ipa_codes.txt.new
+`;
 
 const latestHash = await md5("file:///tmp/ipa_codes.txt.new");
 
@@ -110,7 +110,8 @@ Run
 curl -sL '${AMMINISTRAZIONI_TXT_URL}' |
   tail -n +2 |
   cut -f1 |
-  LC_COLLATE=C sort > data/it/ipa_codes.txt
+  LC_COLLATE=C sort > data/it/ipa_codes.txt &&
+  go-bindata -pkg assets -o assets/assets.go data/...
 \`\`\`
 to generate an updated \`ipa_codes.txt\`.
 