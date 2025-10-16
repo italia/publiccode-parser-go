96e0f40887dabd60a8e704fb6b43f5c7afdf1544
Fabio Bonelli <fb@fabiobonelli.it>
2025-10-16 10:44:21 +0200

    test: fix valid_with_warnings/valid.minimal.v0.4.yml

--- publiccode.go
+++ publiccode.go
@@ -1,7 +1,7 @@
 package publiccode
 
 // SupportedVersions lists the publiccode.yml versions this parser supports.
-var SupportedVersions = []string{"0", "0.2", "0.2.0", "0.2.1", "0.2.2", "0.3", "0.3.0", "0.4", "0.4.0", "0.5", "0.5.0"}
+var SupportedVersions = []string{"0", "0.2", "0.2.0", "0.2.1", "0.2.2", "0.3", "0.3.0", "0.4", "0.4.0", "0.5.0", "0.5"}
 
 type PublicCode interface {
 	Version() uint
