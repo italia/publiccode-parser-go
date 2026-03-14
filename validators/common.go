1287995c775543ee3080012b335c29f5bbe8ae8a
Fabio Bonelli <fb@fabiobonelli.it>
2026-03-14 18:09:03 +0100

    perf: compile regexps once at package level

--- validators/common.go
+++ validators/common.go
@@ -13,6 +13,9 @@ import (
 	"github.com/rivo/uniseg"
 )
 
+// Reference: https://github.com/jshttp/media-typer/
+var reMIMEType = regexp.MustCompile(`^ *([A-Za-z0-9][A-Za-z0-9!#$&^_-]{0,126})/([A-Za-z0-9][A-Za-z0-9!#$&^_.+-]{0,126}) *$`)
+
 func isIso3166Alpha2LowerOrUpper(fl validator.FieldLevel) bool {
 	str := fl.Field().String()
 	validate := validator.New(validator.WithRequiredStructEnabled())
@@ -131,8 +134,5 @@ func isSPDXExpression(fl validator.FieldLevel) bool {
 
 // isMIMEType checks whether the string in input is a well formed MIME type or not.
 func isMIMEType(fl validator.FieldLevel) bool {
-	// Reference: https://github.com/jshttp/media-typer/
-	re := regexp.MustCompile("^ *([A-Za-z0-9][A-Za-z0-9!#$&^_-]{0,126})/([A-Za-z0-9][A-Za-z0-9!#$&^_.+-]{0,126}) *$")
-
-	return re.MatchString(fl.Field().String())
+	return reMIMEType.MatchString(fl.Field().String())
 }
