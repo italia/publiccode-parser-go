44cfbef854aea0cd845704a3ee77ae07f4153653
Fabio Bonelli <fb@fabiobonelli.it>
2026-03-17 09:41:33 +0100

    perf: misc optimization fixes

--- validators/common.go
+++ validators/common.go
@@ -16,9 +16,11 @@ import (
 // Reference: https://github.com/jshttp/media-typer/
 var reMIMEType = regexp.MustCompile(`^ *([A-Za-z0-9][A-Za-z0-9!#$&^_-]{0,126})/([A-Za-z0-9][A-Za-z0-9!#$&^_.+-]{0,126}) *$`)
 
+var sharedValidator = validator.New(validator.WithRequiredStructEnabled())
+
 func isIso3166Alpha2LowerOrUpper(fl validator.FieldLevel) bool {
 	str := fl.Field().String()
-	validate := validator.New(validator.WithRequiredStructEnabled())
+	validate := sharedValidator
 
 	// Either all lower or all upper, don't allow mixed case
 	if str != strings.ToLower(str) && str != strings.ToUpper(str) {
@@ -47,7 +49,7 @@ func uMin(fl validator.FieldLevel) bool {
 // go-playground/validator doesn't support `http_url` validations on non
 // strings yet.
 func isHTTPURL(fl validator.FieldLevel) bool {
-	validate := validator.New(validator.WithRequiredStructEnabled())
+	validate := sharedValidator
 
 	if stringer, ok := fl.Field().Interface().(fmt.Stringer); ok {
 		err := validate.Var(stringer.String(), "http_url")
@@ -62,7 +64,7 @@ func isHTTPURL(fl validator.FieldLevel) bool {
 // go-playground/validator doesn't support `url` validations on non
 // strings yet.
 func isURL(fl validator.FieldLevel) bool {
-	validate := validator.New(validator.WithRequiredStructEnabled())
+	validate := sharedValidator
 
 	if stringer, ok := fl.Field().Interface().(fmt.Stringer); ok {
 		err := validate.Var(stringer.String(), "url")
@@ -110,7 +112,7 @@ func isOrganisationURI(fl validator.FieldLevel) bool {
 
 // Custom validator to work around https://github.com/go-playground/validator/issues/1260
 func bcp47_keys(fl validator.FieldLevel) bool {
-	validate := validator.New(validator.WithRequiredStructEnabled())
+	validate := sharedValidator
 
 	if fl.Field().Kind() != reflect.Map {
 		//nolint:forbidigo // If we hit this, it's a programming error caught at runtime, it's good to panic.
