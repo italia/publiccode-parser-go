fb3efbb2162f2fd5a115187a0f284c3bf4ccb7e0
Fabio Bonelli <fb@fabiobonelli.it>
2026-03-18 06:57:16 +0100

    refactor: replace go-oembed and shrink oEmbed JSON
    Don't use go-oembed, which seems unmaintained and shrink
    oEmbed data from 131KiB to 23KiB.
    
    Fix #177.

--- data/embed.go
+++ data/embed.go
@@ -3,8 +3,8 @@ package data
 import _ "embed"
 
 var (
-	//go:embed oembed_providers.json
-	OembedProviders []byte
+	//go:embed oembed_schemes.json
+	OembedSchemes []byte
 	//go:embed it/ipa_codes.txt
 	ItIpaCodes string
 )
